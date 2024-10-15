49700ad888c0269a45d245d4dc17c9c9d0dab289
Hester74 <Shawn_Hester@hotmail.com>
2024-10-15 13:29:12 -0400

    Update calibrate.py
    Changed comment back from testing branch

--- calibrate.py
+++ calibrate.py
@@ -62,7 +62,7 @@ antibandingOpts = {
 
 def create_blank(width, height, rgb_color=(0, 0, 0)):
     """Create new image(numpy array) filled with certain color in RGB"""
-    # Create black blank image Hester
+    # Create black blank image
     image = np.zeros((height, width, 3), np.uint8)
 
     # Since OpenCV uses BGR, convert the color first
